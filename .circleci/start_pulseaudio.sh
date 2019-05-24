4728b7524365be23429d7024dfa756d27bba5456
Wellington Wallace <wellingtonwallace@gmail.com>
2019-05-23 22:25:41 -0300

    improved circleci tests

--- .circleci/start_pulseaudio.sh
+++ .circleci/start_pulseaudio.sh
@@ -4,11 +4,11 @@ set -e # fail on any error
 
 pulseaudio -D --exit-idle-time=-1
 
-# pactl load-module module-null-sink sink_name=DummyOutput sink_properties=device.description="Virtual_Dummy_Output"
-# pactl load-module module-null-sink sink_name=MicOutput sink_properties=device.description="Virtual_Microphone_Output"
+pactl load-module module-null-sink sink_name=DummyOutput sink_properties=device.description="Virtual_Dummy_Output"
+pactl load-module module-null-sink sink_name=MicOutput sink_properties=device.description="Virtual_Microphone_Output"
 
-# pacmd set-default-source MicOutput.monitor
-# pacmd set-default-sink DummyOutput
+pacmd set-default-source MicOutput.monitor
+pacmd set-default-sink DummyOutput
 
 pactl list sinks
 pactl list sources
